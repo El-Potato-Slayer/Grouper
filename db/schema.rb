2b93092735a0e188d1d1189f93c6f77706fa386c
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-01-29 07:55:16 +0200

    restructured migration for postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_194212) do
+ActiveRecord::Schema.define(version: 2021_01_29_054909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2021_01_24_194212) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_194212) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(version: 2021_01_24_194212) do
   end
 
   create_table "group_expenses", force: :cascade do |t|
-    t.integer "expense_id", null: false
-    t.integer "group_id", null: false
+    t.bigint "expense_id", null: false
+    t.bigint "group_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["expense_id"], name: "index_group_expenses_on_expense_id"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_194212) do
   end
 
   create_table "groups", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
