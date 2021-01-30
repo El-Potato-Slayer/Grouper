f6397649743a5b996780d879a5460ef524e2e0b2
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-01-31 01:30:36 +0200

    added more tests

--- spec/controllers/groups_request_spec.rb
+++ spec/controllers/groups_request_spec.rb
@@ -2,25 +2,11 @@ require 'rails_helper'
 require_relative '../support/devise'
 
 RSpec.describe GroupsController, type: :controller do
-  login_user
+  # user = FactoryBot.create(:user)
   
-  # let(:valid_attributes) {
-  #   { :name => "Test title" }
-  # }
-
-  # let(:valid_session) { {} }
-
-  # describe "GET #index" do
-  #   it 'returns a success response' do
-  #     Group.create! valid_attributes
-  #     get :index, params: {}, session: valid_session
-  #     expect(response).to be_successful
-  #   end
-  # end
-
   describe "GET #index" do
-    # login_user
-    
+    # login_as(user, :scope => :user)
+    login_user
     it "returns a success response" do
       get :index
       expect(response).to be_successful
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,15 +1,17 @@
 FactoryBot.define do
   factory :user do
     # id {1}
-    # sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample.com"}
+    sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample.com"}
     name {'John'}
-    email {'john@user.com'}
+    # email {'john@user.com'}
     password { "123456"}
     password_confirmation { "123456"}
   end
+  factory :group do
+    association :user
+    name {'Craigslist'}
+  end
   factory :expense do
-    # id {1}
-    # sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample.com"}
     author_id {1}
     name {'Plushy'}
     amount {20}
