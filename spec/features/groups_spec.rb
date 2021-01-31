78cbdc26cc40c8d6bb174a9e0463f291fad4c342
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-01-31 23:29:55 +0200

    fixed titles and rspec errors

--- spec/features/groups_spec.rb
+++ spec/features/groups_spec.rb
@@ -10,7 +10,7 @@ RSpec.feature "Groups", type: :feature do
       within('form') do
         fill_in 'group_name', with: 'Test'
       end
-      click_button 'Create group'
+      click_button 'Submit'
       expect(page).to have_content('Group was successfully created.')
     end
 
@@ -18,7 +18,7 @@ RSpec.feature "Groups", type: :feature do
       user = FactoryBot.create(:user)
       login_as(user, :scope => :user)
       visit new_group_path
-      click_button 'Create group'
+      click_button 'Submit'
       expect(page).to have_content("Name can't be blank")
     end
   end
