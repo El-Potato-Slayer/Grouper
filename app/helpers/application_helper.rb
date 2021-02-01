dd8e21d763ab8bca25079cc47a9f18dea4c81b14
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-02-01 21:31:47 +0200

    fixed linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,13 @@
 module ApplicationHelper
   def logout_link
-    if user_signed_in?
-      link_to 'Logout', destroy_user_session_path, method: :delete, class: "py-1 w-full"
-    end
+    link_to 'Logout', destroy_user_session_path, method: :delete, class: 'py-1 w-full' if user_signed_in?
   end
 
   def login_link
-    unless user_signed_in?
-      link_to 'Login', new_user_session_path, class: "px-3 py-1"
-    end
+    link_to 'Login', new_user_session_path, class: 'px-3 py-1' unless user_signed_in?
   end
 
   def register_link
-    unless user_signed_in?
-      link_to 'Register',  new_user_registration_path, class: "px-3 py-1"
-    end
+    link_to 'Register', new_user_registration_path, class: 'px-3 py-1' unless user_signed_in?
   end
 end
