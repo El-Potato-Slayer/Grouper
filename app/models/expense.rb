1413ad9a635c3f101d1d86fb2909f357b26500b7
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-02-01 15:48:18 +0200

    removed all occurernces of N+1 queries

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -5,7 +5,7 @@ class Expense < ApplicationRecord
   has_many :groups, through: :group_expenses
 
   scope :grouped, -> { includes(:groups).where.not(groups: { id: nil }).order('expenses.created_at DESC') }
-  scope :ungrouped, -> { includes(:groups).where.missing(:groups).order('expenses.created_at DESC') }
+  scope :ungrouped, -> { where.missing(:groups).order('expenses.created_at DESC') }
 
   validates :author_id, presence: true
   validates :name, presence: true
