71525522c8a36055b52cbfb4c9753c6a8baab65b
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-02-01 21:15:44 +0200

    moved logic from navbar to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def logout_link
+    if user_signed_in?
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: "py-1 w-full"
+    end
+  end
+
+  def login_link
+    unless user_signed_in?
+      link_to 'Login', new_user_session_path, class: "px-3 py-1"
+    end
+  end
+
+  def register_link
+    unless user_signed_in?
+      link_to 'Register',  new_user_registration_path, class: "px-3 py-1"
+    end
+  end
 end
